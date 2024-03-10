0e0c080d9a81de229cb171f355743bf5486c1dc2
Ahsan <ahsan.qureshi8@gmail.com>
2024-03-10 12:18:57 -0400

    VP version of WeSplit complete

--- WeSplitVP/ContentView.swift
+++ WeSplitVP/ContentView.swift
@@ -10,14 +10,51 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    @State private var amount: Double = 0.0
+    @State private var peopleCount: Int = 2
+    @State private var tipPercentage: Int = 20
+    
+    let currencyCode = Locale.current.currency?.identifier ?? "USD"
+   
+    var total: Double {
+        amount * Double(100 + tipPercentage) / 100
+    }
+    var amountPerPerson: Double {
+        total / Double(peopleCount)
+    }
+    
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+        NavigationStack {
+            Spacer()
+            Section("Bill Amount") {
+                TextField("Amount",
+                          value: $amount,
+                          format: .currency(code: currencyCode))
+                Picker("People Count", selection: $peopleCount) {
+                    ForEach(1..<99, id: \.self) {
+                        Text("\($0) People")
+                    }
+                }
+            }
+            Spacer()
+            Section("Tip Percentage") {
+                Picker("Tip Perecentage", selection: $tipPercentage) {
+                    ForEach(1..<101, id: \.self) {
+                        Text($0, format: .percent)
+                    }
+                }
+                .pickerStyle(.navigationLink)
+            }
+            Spacer()
+            Section("Total") {
+                Text(total, format: .currency(code: currencyCode))
+            }
+            Spacer()
+            Section("Amount per person") {
+                Text(amountPerPerson, format: .currency(code: currencyCode))
+            }
+            Spacer()
         }
-        .padding()
     }
 }
 
